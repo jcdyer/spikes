edea99aed941813511baa41ab9163fd2922faab3
J. Cliff Dyer <jcd@sdf.org>
2020-12-10 18:57:16 -0500

    Add binary module to depend on github.com/jcdyer/spikes/go20201210

---
+++ go-2020-12-10-bin/go.mod
@@ -0,0 +1,5 @@
+module github.com/jcdyer/spikes/go-2020-12-10-bin
+
+go 1.15
+
+require github.com/jcdyer/spikes/go20201210 v1.0.1
---
+++ go-2020-12-10-bin/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jcdyer/spikes/go20201210/go2020"
+)
+
+func main() {
+	fmt.Println("ObjectID:", go2020.GetId())
+}
